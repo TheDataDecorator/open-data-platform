a8c7813e542f5bffd76d0dd8fe32e1eb12c6e25b
TheDataDecorator <marc@thedatadecorator.com>
2026-04-01 23:32:39 +0100

    adding customers sample pipeline

---
+++ airflow/dags/customers_pipeline.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+import os
+
+# One-Touch: We pull the password from the environment so it's never in the code
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    'customer_data_transform',
+    default_args=default_args,
+    description='Triggers dbt to transform raw customers data to Silver layer',
+    schedule_interval=None, # Triggered manually or via NiFi API
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+    tags=['dbt', 'postgres'],
+) as dag:
+
+    # Task 1: Check dbt connection (Debug step)
+    debug_dbt = BashOperator(
+        task_id='debug_dbt_connection',
+        bash_command='cd /opt/airflow/dbt && dbt debug --profiles-dir .',
+        env={'POSTGRES_PASSWORD': POSTGRES_PASSWORD}
+    )
+
+    # Task 2: Run Transformations (Bronze -> Silver)
+    run_dbt = BashOperator(
+        task_id='run_dbt_transformations',
+        bash_command='cd /opt/airflow/dbt && dbt run --profiles-dir .',
+        env={'POSTGRES_PASSWORD': POSTGRES_PASSWORD}
+    )
+
+    debug_dbt >> run_dbt
---
+++ dbt/models/staging/stg_customers.sql
@@ -0,0 +1,22 @@
+-- This takes the 500MB raw table and cleans it up
+WITH raw_data AS (
+    SELECT * FROM {{ source('raw', 'raw_customers') }}
+)
+
+SELECT 
+    -- 1. Clean up column names
+    cast("Index" AS INTEGER) AS customer_index,
+    "Customer Id" AS customer_id,
+    TO_TIMESTAMP("Subscription Date", 'YYYY/MM/DD') AS subscription_date,
+	"First Name", 
+	"Last Name", 
+	"Company", 
+	"City", 
+	"Country", 
+	"Phone 1", 
+	"Phone 2", 
+	"Email", 
+	"Website"
+
+FROM raw_data
+WHERE "Index" IS NOT NULL
